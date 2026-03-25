0a19147e1f6ae9fdab59365afe514beca6ab5b32
liviacrema-237 <Cremalivia31@gmail.com>
2026-03-25 10:29:00 -0300

    Atividade aninhada

--- condicao-complexa-OU/script.js
+++
@@ -1,9 +0,0 @@
-let idade = Number(prompt("Digite sua idade:"));
-let estudante = Number(prompt("É estudante? (sim ou nao"));
-let professor = prompt(prompt("É professor? (sim ou nao"));
-//condição com ou
-if (idade >= 60 || estudante === "sim" || professor === "sim") {
-    alert("Tem direito ao desconto")
-} else {
-    alert("Não tem desconto");
-}
--- condicao-simples/condição-simples/script.js
+++
@@ -1,9 +0,0 @@
-let idade = Number(prompt("Insira sua idade"));
-
-if (idade >=18) {
-    alert("Voce é maior de idade");
-} else {
-    alert("Voce é menor de idade");
-}
-
-      
